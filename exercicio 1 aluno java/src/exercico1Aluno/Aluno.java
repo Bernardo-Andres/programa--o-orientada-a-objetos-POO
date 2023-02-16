17c2717ce51572c70adb4787a9cdfc9f485a5d5f
Bernardo Andrés <80061065+Bernardo-Andres@users.noreply.github.com>
2023-02-16 12:32:57 -0300

    terminei o código

--- exercicio 1 aluno java/src/exercico1Aluno/Aluno.java
+++ exercicio 1 aluno java/src/exercico1Aluno/Aluno.java
@@ -1,6 +1,10 @@
 package exercico1Aluno;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.Locale;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class Aluno {
 	
@@ -8,14 +12,17 @@ private String nome;
 private String CPF;
 private Date dataNasc;
 private String email;
+private Integer idade;
 	
-public Aluno (String nome, String CPF, Date dataNasc, String email)
+public Aluno (String nome, String CPF, Date dataNasc, String email) throws Exception
 	{
 		super();
 		this.nome = nome;
 		this.CPF = CPF;
 		this.dataNasc = dataNasc;
 		this.email = email;	
+		this.setIdade(0);
+		idadeAluno();
 	}
 
 //get serve para recuperar um determinado atributo de um objeto
@@ -35,8 +42,10 @@ public void setCPF (String CPF) {
 	this.CPF = CPF;
 }
 
-public Date getdataNasc() {
-	return dataNasc;
+public String getdataNasc() {
+	SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+	
+	return formato.format(dataNasc);
 }
 
 public void setdataNasc(Date dataNasc) {
@@ -51,8 +60,32 @@ public void setemail(String email) {
 	this.email = email;
 }
 
-public Integer idadeAluno() {
-	Integer idade = 0;
+public void idadeAluno() throws Exception {
+	
+	//SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+    //Date firstDate = sdf.parse("04/22/2020");
+    //Date secondDate = sdf.parse("04/27/2020");
+	
+	Date firstDate = this.dataNasc;
+	
+	DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	Date secondDate = new Date();
+	
+    long diff = secondDate.getTime() - firstDate.getTime();
+
+    TimeUnit time = TimeUnit.DAYS; 
+    
+    long diffrence = time.convert(diff, TimeUnit.MILLISECONDS) / 365;    
+    
+	this.setIdade((int) (long) diffrence);
+	
+	}
+
+public Integer getIdade() {
 	return idade;
 }
+
+private void setIdade(Integer idade) {
+	this.idade = idade;
+	}
 }
--- exercicio 1 aluno java/src/exercico1Aluno/Principal.java
+++ exercicio 1 aluno java/src/exercico1Aluno/Principal.java
@@ -1,39 +1,75 @@
 package exercico1Aluno;
 
-import java.text.ParseException;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.ArrayList;
+
+
 
 public class Principal {
-	public static void main(String[] args) throws ParseException{
+	public static void main(String[] args) throws Exception{
 		//Aluno aluno = new aluno(); //Criando um objeto aluno
+		try {
+			
+			ArrayList<Aluno> listaDeAlunos = new ArrayList<Aluno>();
+			
+			SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+			
+			Date dataAluno = formato.parse("02/11/2002");
+			Aluno aluno1 = new Aluno("Joaquim", "105.888.111-11", dataAluno, "joaquim@gmail.com");
+			listaDeAlunos.add(aluno1);
+			
+			dataAluno = formato.parse("02/09/1997");
+			aluno1 = new Aluno("Bernardo", "105.837.106-11", dataAluno, "br.abar33@gmail.com");
+			listaDeAlunos.add(aluno1);
+			
+			//SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+			dataAluno = formato.parse("12/09/1998");
+			aluno1 = new Aluno("Carlos", "125.837.186-12", dataAluno, "carlos@gmail.com");
+			listaDeAlunos.add(aluno1);
+			
+			//SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+			dataAluno = formato.parse("02/11/2000");
+			aluno1 = new Aluno("Joaquim", "105.888.111-11", dataAluno, "joaquim@gmail.com");
+			listaDeAlunos.add(aluno1);
+			
+			aluno1 = listaDeAlunos.get(0);
+			
+			for(int i=0; i < listaDeAlunos.size(); i++) {
+				System.out.println("nome: "+ listaDeAlunos.get(i).getnome());
+				System.out.println("CPF: " + listaDeAlunos.get(i).getCPF());
+				//System.out.println("Data de nascimento: "+ formato.format(dataAluno));
+				System.out.println("Data de nascimento: "+ listaDeAlunos.get(i).getdataNasc());
+				System.out.println("Idade: "+ listaDeAlunos.get(i).getIdade());
+				System.out.println("Email: "+ listaDeAlunos.get(i).getemail());
+				
+				if(listaDeAlunos.get(i).getIdade() > aluno1.getIdade()) {
+					aluno1 = listaDeAlunos.get(i);
+					
+				}
+			}
+			
+			System.out.println("Aluno mais velho: ");
+			System.out.println("nome: "+ aluno1.getnome());
+			System.out.println("CPF: " + aluno1.getCPF());
+			//System.out.println("Data de nascimento: "+ formato.format(dataAluno));
+			System.out.println("Data de nascimento: "+ aluno1.getdataNasc());
+			System.out.println("Idade: "+ aluno1.getIdade());
+			System.out.println("Email: "+ aluno1.getemail());
+			
+			
+		} catch(Exception ex)  {
+			System.out.println("Erro ao montar data ");
+		}
 		
-		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-		Date dataAluno = formato.parse("02/09/1997");
-		Aluno aluno1 = new Aluno("Bernardo", "105.837.106-11", dataAluno, "br.abar33@gmail.com");
-		
-		//SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-		Date dataAluno1 = formato.parse("12/09/1998");
-		Aluno aluno2 = new Aluno("Carlos", "125.837.186-12", dataAluno1, "carlos@gmail.com");
-		
-		//SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-		Date dataAluno2 = formato.parse("02/11/2000");
-		Aluno aluno3 = new Aluno("Joaquim", "105.888.111-11", dataAluno2, "joaquim@gmail.com");
-		
-		System.out.println("nome: "+ aluno1.getnome());
-		System.out.println("CPF: " + aluno1.getCPF());
-		System.out.println("Data de nascimento: "+ formato.format(dataAluno));
-		System.out.println("Email: "+ aluno1.getemail());
 		
 		
-		System.out.println("nome: "+ aluno2.getnome());
-		System.out.println("CPF: " + aluno2.getCPF());
-		System.out.println("Data de nascimento: "+ formato.format(dataAluno1));
-		System.out.println("Email: "+ aluno2.getemail());
 		
-		System.out.println("nome: "+ aluno3.getnome());
-		System.out.println("CPF: " + aluno3.getCPF());
-		System.out.println("Data de nascimento: "+ formato.format(dataAluno2));
-		System.out.println("Email: "+ aluno3.getemail());
+		
+		
+		
+		
+	
 	}
 }
