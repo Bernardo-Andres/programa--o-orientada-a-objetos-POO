8ed8e85617d197bc7e6b44b1cedf34ab68e84c88
Bernardo Andrés <80061065+Bernardo-Andres@users.noreply.github.com>
2023-03-12 17:35:58 -0300

    exercicio feito

--- Exercicio5/src/Exercicio5_BernardoAndrés/Principal.java
+++ Exercicio5/src/Exercicio5_BernardoAndrés/Principal.java
@@ -11,6 +11,38 @@ import java.text.SimpleDateFormat;
 public class Principal {
 	static SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 	
+	
+	
+	
+	public static Double main2(ArrayList<Produtos> listaProdutos, int v []) {
+		int i = 0;
+		
+		Double somatorioProdutos = 0.0;
+		
+		for(i = 0; i < v.length; i++) {
+			//System.out.println("Vetor que foi passado: " + v[i]);
+		}
+		
+		
+		for(Produtos pro: listaProdutos) {
+			int j = 0;
+			for(j = 0; j < v.length; j++) {
+				if(pro.getId() == v[j]) {
+					//System.out.println("O produto: " + pro.getNome() + " tem o id: " + pro.getId() + " e o vetor: " + v[j]);
+					somatorioProdutos = somatorioProdutos + pro.getPreco_unitario();
+				}
+			}
+		}
+		
+		//System.out.println("A soma dos produtos é de: " + somatorioProdutos);
+		
+		return somatorioProdutos;
+		
+	}
+	
+	
+	
+	
 	public static void main(String[] args) throws ParseException {
 		
 		ArrayList<Fornecedor> listaFornecedor = new ArrayList<Fornecedor>();
@@ -23,6 +55,14 @@ public class Principal {
 		
 		ArrayList<VendaProdutos> listaVendaProdutos = new ArrayList<VendaProdutos>();
 		
+		Double valorTotal;
+		
+		List<String> clientesFuncionarios = new ArrayList<String>();
+		
+		List<String> clientesFornecedores = new ArrayList<String>();
+		
+		List<String> clientesClientes = new ArrayList<String>();		
+		
 		List<String> telefones = new ArrayList<String>();
 		
 		telefones.add("741963258");
@@ -30,7 +70,8 @@ public class Principal {
 		
 		
 		Fornecedor fornecedor1 = new Fornecedor ("Roberto", "robertofazendinha@gmail.com", telefones ,"rua 12", "22/09/1990", "895623147");
-		listaFornecedor.add(fornecedor1);
+		listaFornecedor.add(fornecedor1);		
+		clientesFornecedores.add("895623147");
 		
 		telefones.clear();
 		
@@ -38,7 +79,8 @@ public class Principal {
 		telefones.add("321456987");
 		
 		Fornecedor fornecedor2 = new Fornecedor ("Claudio", "Claudiofazendinha@gmail.com", telefones ,"rua 15", "20/09/1991", "741258963");
-		listaFornecedor.add(fornecedor2);
+		listaFornecedor.add(fornecedor2);		
+		clientesFornecedores.add("741258963");
 		
 		telefones.clear();
 		
@@ -48,13 +90,13 @@ public class Principal {
 		Produtos produtos = new Produtos (01, "Placa mãe", 1267.00, fornecedor1);
 		listaProdutos.add(produtos);
 		
-		produtos = new Produtos (01, "Placa de video", 1599.00, fornecedor1);
+		produtos = new Produtos (02, "Placa de video", 1599.00, fornecedor1);
 		listaProdutos.add(produtos);
 		
-		produtos = new Produtos (02, "Memória RAM", 469.00, fornecedor2);
+		produtos = new Produtos (03, "Memória RAM", 469.00, fornecedor2);
 		listaProdutos.add(produtos);
 		
-		produtos = new Produtos (02, "HD", 260.00, fornecedor2);
+		produtos = new Produtos (04, "HD", 260.00, fornecedor2);
 		listaProdutos.add(produtos);
 		
 		
@@ -63,7 +105,8 @@ public class Principal {
 		telefones.add("984878118");
 		
 		Cliente cliente = new Cliente("Bernardo", "br.abar33@gmail.com", telefones, "Av. olegario", "02/09/1997", "10583710688");
-		listaCliente.add(cliente);
+		listaCliente.add(cliente);		
+		clientesClientes.add("10583710688");
 		
 		telefones.clear();
 		
@@ -71,7 +114,8 @@ public class Principal {
 		telefones.add("33333342");
 		
 		cliente = new Cliente("Joao Bonato", "jaozin@gmail.com", telefones, "Rua 7", "02/09/1997", "10689710511");
-		listaCliente.add(cliente);
+		listaCliente.add(cliente);		
+		clientesClientes.add("10689710511");
 		
 		telefones.clear();
 		
@@ -80,51 +124,40 @@ public class Principal {
 		
 		
 		cliente = new Cliente("Joao Arthur", "jaozinArthur@gmail.com", telefones, "Rua 20", "17/07/1998", "45493826691");
-		listaCliente.add(cliente);
-		
-		
-		
+		listaCliente.add(cliente);		
+		clientesClientes.add("45493826691");		
 		
 		
 		telefones.add("988231298");
 		telefones.add("33325896");	
 		
 		Funcionario funcionario = new Funcionario("Hanna", "hanna@gmail.com", telefones, "Rua 13", format.parse("22/10/2000"), "vendedor", "10690877", format.parse("22/06/1990"));
-		listaFuncionario.add(funcionario);
+		listaFuncionario.add(funcionario);		
+		clientesFuncionarios.add("10690877");
 		
 		telefones.clear();
 		
 		telefones.add("99998528");
 		
-		funcionario = new Funcionario("Mauro", "mauro@gmail.com", telefones, "Rua 19", format.parse("22/10/2000"), "vendedor", "10690877", format.parse("22/06/1990"));
-		listaFuncionario.add(funcionario);
-		
-		
-		
-		
-		
-		for(Produtos prod: listaProdutos ) {
-			System.out.println(prod.getNome()+"  "+ prod.getForn().getNome());
-		}
-		
-		for(Cliente cli: listaCliente) {
-			System.out.println(cli.getNome() + "  "+ cli.getTelefone());
-		}
-		
-		for(Funcionario func: listaFuncionario) {
-			System.out.println(func.getNome());
-			System.out.println(func.getDataadm());
-		}
-		
-		
-		
-		
+		funcionario = new Funcionario("Mauro", "mauro@gmail.com", telefones, "Rua 19", format.parse("22/10/2000"), "vendedor", "10690888", format.parse("22/06/1990"));
+		listaFuncionario.add(funcionario);		
+		clientesFuncionarios.add("10690888");
 		
 			
+		valorTotal = main2(listaProdutos, new int[] {01,02,03});
+		
+		VendaProdutos vendaProdutos = new VendaProdutos(listaCliente, listaProdutos, listaFornecedor, listaFuncionario, valorTotal, valorTotal);
+		listaVendaProdutos.add(vendaProdutos);
+		vendaProdutos.getValorFinalComDesconto(vendaProdutos.getCliente().get(1).getCpf());
+
+		
+		valorTotal = main2(listaProdutos, new int[]{03,04});
+		vendaProdutos = new VendaProdutos(listaCliente, listaProdutos, listaFornecedor, listaFuncionario, valorTotal, valorTotal);
+		listaVendaProdutos.add(vendaProdutos);
+		vendaProdutos.getValorFinalComDesconto(vendaProdutos.getFuncionario().get(0).getCpf());
+	
 		
 		
-		
-		
-		
+				
 	}
 }
--- Exercicio5/src/Exercicio5_BernardoAndrés/VendaProdutos.java
+++ Exercicio5/src/Exercicio5_BernardoAndrés/VendaProdutos.java
@@ -1,77 +1,155 @@
 package Exercicio5_BernardoAndrés;
 
+import java.util.ArrayList;
+
 public class VendaProdutos {
-private Cliente cliente;
-private Produtos produto;
-private Fornecedor fornecedor;
-private Funcionario funcionario;
-private Double valorTotal;
-private Double valorFinal;
+	private ArrayList<Cliente> cliente;
+	private ArrayList<Produtos> produtos;
+	private ArrayList<Fornecedor> fornecedor;
+	private ArrayList<Funcionario> funcionario;
+	private Double valorTotal;
+	private Double valorFinal;
+	
+	
+	
+	public VendaProdutos(ArrayList<Cliente> cliente, ArrayList<Produtos> produtos, ArrayList<Fornecedor> fornecedor, ArrayList<Funcionario> funcionario, Double valorTotal,
+			Double valorFinal) {
+		super();
+		this.cliente = cliente;
+		this.produtos = produtos;
+		this.fornecedor = fornecedor;
+		this.funcionario = funcionario;
+		this.valorTotal = valorTotal;
+		this.valorFinal = valorFinal;
+	}
 
 
-public VendaProdutos(Cliente cliente, Produtos produto, Fornecedor fornecedor, Funcionario funcionario,
-		Double valorTotal, Double valorFinal) {
-	super();
-	this.setCliente(cliente);
-	this.setProduto(produto);
-	this.setFornecedor(fornecedor);
-	this.setFuncionario(funcionario);
-	this.setValorTotal(valorTotal);
-	this.setValorFinal(valorFinal);
-}
+	public ArrayList<Cliente> getCliente() {
+		return cliente;
+	}
 
-public Cliente getCliente() {
-	return cliente;
-}
 
-public void setCliente(Cliente cliente) {
-	this.cliente = cliente;
-}
+	public void setCliente(ArrayList<Cliente> cliente) {
+		this.cliente = cliente;
+	}
 
-public Produtos getProduto() {
-	return produto;
-}
 
-public void setProduto(Produtos produto) {
-	this.produto = produto;
-}
+	public ArrayList<Fornecedor> getFornecedor() {
+		return fornecedor;
+	}
 
-public Fornecedor getFornecedor() {
-	return fornecedor;
-}
 
-public void setFornecedor(Fornecedor fornecedor) {
-	this.fornecedor = fornecedor;
-}
+	public void setFornecedor(ArrayList<Fornecedor> fornecedor) {
+		this.fornecedor = fornecedor;
+	}
 
-public Funcionario getFuncionario() {
-	return funcionario;
-}
 
-public void setFuncionario(Funcionario funcionario) {
-	this.funcionario = funcionario;
-}
+	public ArrayList<Produtos> getProduto() {
+		return produtos;
+	}
 
-public Double getValorTotal() {
-	return valorTotal;
-}
 
-public void setValorTotal(Double valorTotal) {
-	this.valorTotal = valorTotal;
-}
+	public void setProduto(ArrayList<Produtos> produto) {
+		this.produtos = produto;
+	}
 
-public Double getValorFinal() {
-	return valorFinal;
-}
 
-public void setValorFinal(Double valorFinal) {
-	this.valorFinal = valorFinal;
-}
+	public ArrayList<Funcionario> getFuncionario() {
+		return funcionario;
+	}
 
-/*public double getValorFinalComDesconto() {
+
+	public void setFuncionario(ArrayList<Funcionario> funcionario) {
+		this.funcionario = funcionario;
+	}
+
+
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+
+
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+
+	public Double getValorFinal() {
+		return valorFinal;
+	}
+
+
+	public void setValorFinal(Double valorFinal) {
+		this.valorFinal = valorFinal;
+	}
+	
+	
+	public void getValorFinalComDesconto(String cpf) {
+		
+		int verificaPessoa = 0;
+		for(Cliente cli: cliente) {
+			if(cli.getCpf() == cpf) {
+				//System.out.println("teste de for");
+				System.out.println("Nome do cliente:" +cli.getNome());
+				System.out.println("Valor total da comrpra: " + valorTotal);
+				System.out.println("Valor Final da compra:" + valorTotal * 0.9);
+				verificaPessoa = 1;
+				//return valorTotal * 10;
+				
+			}
+		}
+		
+		
+		for(Funcionario fun: funcionario) {
+			if(fun.getCpf() == cpf) {
+				//System.out.println("teste de for");
+				System.out.println("Nome do Funcionario: " + fun.getNome());
+				System.out.println("Valor total da comrpra: " + valorTotal);
+				System.out.println("Valor Final da compra:" + valorTotal * 0.8);
+				verificaPessoa = 1;
+
+				//return valorTotal * 40;
+
+			}
+		}
+		
+		
+		
+		for(Fornecedor forn: fornecedor) {
+			if(forn.getCnpj() == cpf) {
+				System.out.println("teste de for");
+				System.out.println("Nome do Fornecedor: " + forn.getNome());
+				System.out.println("Valor total da comrpra: " + valorTotal);
+				System.out.println("Valor Final da compra:" + valorTotal * 0.8);
+				verificaPessoa = 1;
+
+
+				//return valorTotal * 1000;
+
+			}
+		}
+		
+		
+		if(verificaPessoa == 0) {
+			System.out.println("Conta do novo cliente: " + valorTotal);
+			System.out.println("Valor final da compra" + valorTotal * 0.95);
+			
+		}
+		
+		
+		System.out.println("-------------------------------------------------------------");
+		
+		
+		
+		
+		//return teste;
+		
+	}
+
+	/*public double getValorFinalComDesconto() {
     if (cliente == null) {
         return valorTotal * 0.95;
-    } else if (cliente instanceof funcionario) {
+    } else if (funcionario instanceof Funcionario) {
         return valorTotal * 0.8;
     } else {
         return valorTotal * 0.9;
